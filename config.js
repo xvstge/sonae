f71ff326651a0bcd32e29d01f6cbeefda4ceef78
Zareth <xvstgex@gmail.com>
2026-06-12 19:47:25 -0400

    Optimizar carga del chat

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 const CHAT_CONFIG = {
   "contactName": "Papá patito 🐥💙",
   "status": "en línea",
-  "loginPhoto": "assets/images/login.jpg",
-  "profilePhoto": "assets/images/perfil.jpg",
+  "loginPhoto": "assets/images/login.jpg?v=6a1d5743ac",
+  "profilePhoto": "assets/images/perfil.jpg?v=6a1d5743ac",
   "mySide": "out",
-  "storageNamespace": "c9b20ed99829"
+  "storageNamespace": "c9b20ed99829",
+  "initialMessages": 400,
+  "messageBatch": 400,
+  "maxRenderedMessages": 1200
 };
